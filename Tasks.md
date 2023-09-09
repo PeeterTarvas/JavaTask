c0fa9a0b77b98768dcce2e588983ba0984d9cf32
PeeterTarvas <peetertarvas@gmail.com>
2023-09-09 18:33:31 +0300

    Front-end updates, need to make child-parent logic work

---
+++ Tasks.md
@@ -0,0 +1,22 @@
+Tasks:
+1. Correct all of the deficiencies in index.html
+
+2. "Sectors" selectbox:
+   2.1. Add all the entries from the "Sectors" selectbox to database - need to add all
+   2.2. Compose the "Sectors" selectbox using data from database - need to make child/parent logic work
+
+3. Perform the following activities after the "Save" button has been pressed:
+   3.1. Validate all input data (all fields are mandatory) - made but company repo needs to have a fix
+   3.2. Store all input data to the database (Name, Sectors, Agree to terms) - debug if company already exists then repo will throw error
+   3.3. Refill the form using stored data - need to add user logic - then reload thse users company
+   3.4. Allow the user to edit his/her own data during the session - puts by the user
+
+
+
+Write us Your best code!
+
+
+
+After completing the tasks, please provide us with:
+1. Full database dump (structure and data)
+2. Source code
--- front/src/app/selector-select/selector-select.component.ts
+++ front/src/app/selector-select/selector-select.component.ts
@@ -12,6 +12,7 @@ export class SelectorSelectComponent {
   sectors : SectorDto[] = [];
   private connectionService: ConnectionService
   protected selectedSector: SectorDto | undefined;
+  protected groupedSectors: Map<number, SectorDto[]> = new Map(); // Grouped sectors by parentId
   @Output() selectedSectorEvent: EventEmitter<SectorDto> = new EventEmitter();
 
   constructor(connectionService: ConnectionService) {
@@ -21,12 +22,31 @@ export class SelectorSelectComponent {
   getSelectedSector() {
     this.selectedSectorEvent.emit(this.selectedSector)
   }
+
+  private groupSectorsByParent() {
+    this.sectors.forEach((sector: SectorDto) => {
+      const parentKey = sector.sectorParentId;
+      if (parentKey !== null) {
+         if (!this.groupedSectors.has(parentKey)) {
+          this.groupedSectors.set(parentKey, []);
+        }
+        this.groupedSectors.get(parentKey)!.push(sector);
+      }
+    });
+  }
   ngOnInit() {
     this.connectionService.get("sector/getAll").pipe(
       map(
         (data: any) => {
           this.sectors = data;
+          this.sectors = this.sectors.sort(x => x.sectorId).reverse();
+          this.groupSectorsByParent();
         })).subscribe();
+  }
+
+  getSectorName(parentId: number): string {
+    const parentSector = this.sectors.find(sector => sector.sectorId === parentId);
+    return parentSector ? parentSector.sectorName : '';
   }
 
 }
